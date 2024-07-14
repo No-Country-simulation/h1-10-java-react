8e3e9b2234dd35a1f0bb8fe903e5dda533fca3a4
davidmedev222 <davidmedev@gmail.com>
2024-07-14 16:43:19 -0300

    feat: add UI of the dashboard nomenclators page

--- frontend/src/app/(dashboard)/dashboard/nomenclators/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/nomenclators/page.tsx
@@ -1,7 +1,11 @@
+import { NomenclatorsTable } from '@/components'
+import { Button } from '@/components/ui/button'
+
 function DashboardNomenclatorsPage() {
   return (
     <main className='flex flex-col gap-4 p-4'>
-      <h1 className='text-lg font-semibold md:text-2xl'>Nomencladores</h1>
+      <Button className='mx-auto'>Crear nomenclador</Button>
+      <NomenclatorsTable />
     </main>
   )
 }

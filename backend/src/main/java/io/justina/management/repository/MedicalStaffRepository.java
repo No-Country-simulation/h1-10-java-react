c096da1d5a703c406af5f111e28385e54fab2281
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-07-14 16:05:27 -0500

    Se agrega Dockerfile

--- backend/src/main/java/io/justina/management/repository/MedicalStaffRepository.java
+++ backend/src/main/java/io/justina/management/repository/MedicalStaffRepository.java
@@ -3,11 +3,16 @@ package io.justina.management.repository;
 import io.justina.management.model.MedicalStaff;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MedicalStaffRepository extends JpaRepository<MedicalStaff, Long> {
     UserDetails findByEmail(String email);
+    @Query("""
+            select m.active
+            from MedicalStaff m
+            where m.id=:id
+            """)
+    Boolean findActiveById(Long id);
 }

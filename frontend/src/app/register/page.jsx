f0d6d87b577b8e0be4fd293750eed645a44e8e1f
Yhordi Choque <yordychoque124@gmail.com>
2024-07-26 18:11:07 -0400

    m

--- frontend/src/app/register/page.jsx
+++ frontend/src/app/register/page.jsx
@@ -1,21 +1,19 @@
 'use client'
- import Register from "@/components/Login/Register"
+import Register from "@/components/Login/Register"
 export default function Pagelogin() {
-
-  //const [Logearme, setLogearme] = useState(true) // true si es para registrase y false para iniciar sesion
-
+  // const [Logearme, setLogearme] = useState(true) // true si es para registrase y false para iniciar sesion
 
   return (
     <div className=' min-h-screen 
     bg-amber-50'>
-      {/**Imagen de fondo que se puede cambiar */}
-      
+      {/** Imagen de fondo que se puede cambiar */}
+
       <section className='max-w-2xl flex flex-col mx-auto pt-10 px-3'>
-      
-      <Register/>
-   {/** <Register isRegister = { Logearme}/>*/}   
+
+        <Register />
+        {/** <Register isRegister = { Logearme}/> */} 
       </section>
-        
+
     </div>
   )
 }

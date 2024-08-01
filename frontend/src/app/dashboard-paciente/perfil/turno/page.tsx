ff883a5a76df8c248c86e624eac5ed815ed83d05
Yhordi Choque <yordychoque124@gmail.com>
2024-07-31 23:24:08 -0400

    Registro de turno del paciente

--- frontend/src/app/dashboard-paciente/perfil/turno/page.tsx
+++ frontend/src/app/dashboard-paciente/perfil/turno/page.tsx
@@ -5,41 +5,117 @@ import { Textarea } from '@/components/ui/textarea'
 import { Toaster } from '@/components/ui/toaster'
 import { toast } from '@/components/ui/use-toast'
 import { zodResolver } from '@hookform/resolvers/zod'
-import React from 'react'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-
+import {ObtenerHora} from '@/app/lib/Date'
 const Formshema = z.object({
   descripcion: z.string().min(15, {
     message: 'Por favor la descripción tiene que ser mayor 15 caracteres'
   }).max(250, {
     message: 'Por favor la descripción tiene que ser menor a 250 caracteres'
-  })
+  }),
+  idMedicalStaff: z.string()
 })
 export default function Turno() {
+  const [datosMedicos, setDatosMedicos] = useState([])
+
   const form = useForm<z.infer<typeof Formshema>>({
     resolver: zodResolver(Formshema),
     defaultValues: {
-      descripcion: ''
+      descripcion: '',
+      idMedicalStaff: ''
     }
   })
-
   function onSubmit( value:z.infer<typeof Formshema>) {
-    console.log('hOLa')
+    const localToken = localStorage.getItem('token' ) ?? ''
+    const [timeString] = ObtenerHora()
+    console.log(timeString)
+    const values = {
+      idPatient: '2',
+      idMedicalStaff: value.idMedicalStaff,
+      reason: 'SEGUIMIENTO',
+      description: value.descripcion,
+      healthCenter: 'Buenos Aires Medical Center',
+      date: `2024-08-02${timeString}.774Z`
+    }
+    console.log(value.idMedicalStaff)
+    try {
+      void fetch('https://backend-justina-deploy.onrender.com/v1/api/appointment/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localToken}`
+        },
+        body: JSON.stringify(values)
+
+      }).then(async respuesta => await respuesta.json())
+      .then( dato => console.log(dato))
+    } catch (error) {
+      console.log(error)
+    }
     toast({
       title: 'Excelente, registraste tu consulta con éxito.',
       description: (
         <pre className='mt-2 w-[340px] rounded-md bg-slate-950 p-4'>
-          <code className='text-white'>{JSON.stringify(value, null, 2)}</code>
+          <p className='text-white text-center'>Puedes ver todos tus citas en sección de Home</p>
         </pre>
       )
     })
   }
+
+  useEffect(()=>{
+    const localToken = localStorage.getItem('token' ?? '')
+
+    try {
+      void fetch('https://backend-justina-deploy.onrender.com/v1/api/medical-staff/getAll',{
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localToken
+        }
+      })
+        .then(async respuesta => await respuesta.json())
+        .then( datoMedicos => setDatosMedicos(datoMedicos))
+    } 
+    catch (eror){
+      console.error(eror)
+    }
+
+  },[])
   return (
     <div className='py-7  max-w-[500px] flex flex-col justify-center items-center bg-black rounded-xl text-white'>
       <h6 className='font-bold text-xl'>Saca tu turno</h6>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className='w-2/3 space-y-6'>
+          
+          <FormField
+            control={form.control}
+            name={'idMedicalStaff'}
+            render={({ field }) => (
+              <FormItem className='sm:col-span-2'>
+                <FormLabel>Medicos de turno</FormLabel>
+                <Select onValueChange={field.onChange} defaultValue={field.value}>
+                  <FormControl>
+                    <SelectTrigger >
+                      <SelectValue placeholder='Elija a un medico' />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {datosMedicos.map((item) => (
+                      <SelectItem className='' key={item.id} value={item.id}>
+                        {item.firstName} {item.lastName}
+                        <span>{item.specialities}</span>
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+      )}
+    />
+
           <FormField
             control={form.control}
             name='descripcion'
